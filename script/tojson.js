91f38ece6b2893345a49b35fe57d930370a5750b
oh16 <ssplas@naver.com>
2020-10-29 12:50:29 +0900

    Update

--- script/tojson.js
+++ script/tojson.js
@@ -1,7 +1,13 @@
 const fs = require('fs'); 
 const readline = require('readline'); 
 const path = require('path');
-const DIRECTORY = 'sta'
+const DIRECTORY = './'
+
+let translatedDir=path.join(DIRECTORY,`ko`);
+let refDir=path.join(DIRECTORY,`ref`);
+fs.mkdirSync(translatedDir, {recursive:true});
+fs.mkdirSync(refDir, {recursive:true});
+
 fs.readdirSync(DIRECTORY).forEach(file => {
   LangToJSON(path.join(DIRECTORY,file));
 });
@@ -11,6 +17,7 @@ function LangToJSON(dir) {
 	if(pathInfo.ext!=".lang") return;
 	let dataInfo=path.join(pathInfo.dir,`ko`, `${pathInfo.name}.json`);
 	let refInfo=path.join(pathInfo.dir, `ref`,`${pathInfo.name}.json`);
+	
 	const file = readline.createInterface({ 
 		input: fs.createReadStream(dir, {encoding:"utf8"}), 
 		output: process.stdout, 
@@ -22,7 +29,7 @@ function LangToJSON(dir) {
 		if(l.includes(`=`)) {
 			temp=l.split(`=`);
 			if(temp[0]!='') {
-				if(ref!=''&&!(temp[1].includes(ref))) {
+				if((ref!=''&&!(temp[1].includes(ref))) || (ref==''&&temp[1]!='')) {
 					data[temp[0]]=temp[1];
 				} else {
 					let key = temp[0].split(`.`);
